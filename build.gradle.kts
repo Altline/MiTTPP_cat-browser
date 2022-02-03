1b9e2a91e5ee188bfa6c84d6f3aec6168540f0aa
Dominik Živko <domy.zivko@gmail.com>
2022-02-03 20:44:14 +0100

    Add coroutines and serialization dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,13 @@ val kotlinReactVersion = "17.0.2-pre.293-kotlin-1.6.10"
 val kotlinStyledVersion = "5.3.3-pre.293-kotlin-1.6.10"
 val kotlinReactRouterVersion = "6.2.1-pre.293-kotlin-1.6.10"
 
+val kotlinCoroutinesVersion = "1.6.0"
+val kotlinSerializationVersion = "1.3.2"
+
 plugins {
     val kotlinVersion: String by System.getProperties()
     kotlin("js") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
 }
 
 group = "altline"
@@ -20,8 +24,11 @@ dependencies {
     testImplementation(kotlin("test"))
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react:$kotlinReactVersion")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:$kotlinReactVersion")
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:$kotlinStyledVersion")
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-router-dom:$kotlinReactRouterVersion")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:$kotlinStyledVersion")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$kotlinCoroutinesVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinSerializationVersion")
 }
 
 kotlin {
