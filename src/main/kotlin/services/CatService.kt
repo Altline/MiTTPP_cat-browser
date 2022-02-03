5776a2edf602c298973ad6d0f7e0756ff3f25282
Dominik Živko <domy.zivko@gmail.com>
2022-02-03 21:31:17 +0100

    Add a check and a console notification for missing API key

--- src/main/kotlin/services/CatService.kt
+++ src/main/kotlin/services/CatService.kt
@@ -8,18 +8,31 @@ import kotlin.js.json
 
 class CatService {
     private val baseAddress = "https://api.thecatapi.com/v1/"
-    private val apiKey by lazy { js("API_KEY").toString() }
+    private val apiKey by lazy {
+        kotlin.runCatching {
+            js("API_KEY").toString()
+        }.onFailure {
+            console.error("The API key is not present in the configuration! Please acquire the key in order to access the cat API.")
+        }.getOrNull()
+    }
 
     suspend fun getBreeds(page: Int, limit: Int = 18): Array<Breed> {
-        val json = getAuthorized("breeds", arrayOf(
-            "limit" to limit.toString(),
-            "page" to page.toString()
-        ))
+        return runCatching {
+            val json = getAuthorized(
+                "breeds", arrayOf(
+                    "limit" to limit.toString(),
+                    "page" to page.toString()
+                )
+            )
 
-        return JSON.parse(json)
+            JSON.parse<Array<Breed>>(json)
+
+        }.getOrDefault(emptyArray())
     }
 
     private suspend fun getAuthorized(location: String, params: Array<Pair<String, String>>): String {
+        check(apiKey != null) { "No API key." }
+
         val urlParams = params.fold("?") { acc, param ->
             acc + param.first + "=" + param.second + "&"
         }.dropLast(1)
